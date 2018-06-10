85bce80989e05c7911b9f65909c582fd4abd2d16
alkis <a.kafkis@gmail.com>
2018-06-10 19:41:35 +0300

    Clock icon

--- app/src/main/java/com/katsuna/clock/info/InfoActivity.java
+++ app/src/main/java/com/katsuna/clock/info/InfoActivity.java
@@ -23,7 +23,7 @@ public class InfoActivity extends KatsunaInfoActivity {
         PackageInfo pInfo;
         try {
             pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
-            mAppIcon.setImageResource(R.mipmap.ic_launcher);
+            mAppIcon.setImageResource(R.drawable.common_clock_icon);
             mAppName.setText(R.string.app_name);
             mAppVersion.setText(getString(R.string.common_version_info, pInfo.versionName));
         } catch (PackageManager.NameNotFoundException e) {
